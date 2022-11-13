de1b64f994ee9557c96f95a33f099f8d7ec3ea3c
Dan Knutson <dan.knutson@gmail.com>
2022-11-13 10:04:22 -0600

    clean up some noise
    * try to clean shutdown audio thread on close
    * remove slashes in album titles when resizing images

--- src/ui/resizer.rs
+++ src/ui/resizer.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use anyhow::Context;
 use camino::{Utf8Path, Utf8PathBuf};
 use flume::{Receiver, TryRecvError};
 use log::error;
@@ -82,10 +83,10 @@ async fn step(
                 }
             };
 
-            let message = match resize(request, &images_directory, db.clone()).await {
+            let message = match resize(&request, &images_directory, db.clone()).await {
                 Ok(resized_image) => Some(ResizerMessage::ResizedImage(resized_image)),
                 Err(e) => {
-                    error!("error resizing image: {e}");
+                    error!("error resizing image: {request:#?} {e}");
                     None
                 }
             };
@@ -119,19 +120,25 @@ fn get_images_directory() -> Option<Utf8PathBuf> {
 }
 
 async fn resize(
-    request: ResizeRequest,
+    request: &ResizeRequest,
     images_directory: &Utf8Path,
     db: SqlitePool,
 ) -> anyhow::Result<ResizedImage> {
-    let image_bytes = load_rgba(&request.source_path)?;
+    let image_bytes = load_rgba(&request.source_path).context("loading original")?;
 
-    let title = request.album_title;
+    let title: String = request
+        .album_title
+        .chars()
+        .filter(|&c| c != '\\' && c != '/')
+        .collect();
     let file_name = format!("{title}_{IMAGE_SIZE}.bmp");
+    let file_name: Utf8PathBuf = file_name.try_into()?;
     let path = images_directory.join(file_name);
 
-    save_rgba(&path, &image_bytes)?;
+    save_rgba(&path, &image_bytes)
+        .with_context(|| format!("saving resized bmp: {path}"))?;
 
-    let mut conn = db.get()?;
+    let mut conn = db.get().context("checking out db connection")?;
     conn.immediate_transaction(|tx| {
         add_resized_image_location(tx, request.album_id, &path)
     })?;
