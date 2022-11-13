15cd759e8fc5b82df7acfa336954907a62313775
Dan Knutson <dan.knutson@gmail.com>
2022-11-13 10:42:30 -0600

    wrap ProjectDirs call

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 use std::sync::Arc;
 
-use camino::Utf8PathBuf;
 use iced::{Application, Settings};
 use parking_lot::Mutex;
 
 use clef::channels::*;
 use clef::db;
-use clef::platform::project_dirs;
+use clef::platform::db_path;
 use clef::ui::{Flags, Ui};
 
 fn main() -> iced::Result {
     pretty_env_logger::init();
 
-    let project_dirs = project_dirs().expect("no project directory path for app found");
-    let db_path = project_dirs.data_local_dir().join("db.sqlite");
-    let db_path: Utf8PathBuf = db_path.try_into().expect("non-utf8 local data directory");
+    let db_path = db_path().expect("no sqlite db path");
     let db_pool = db::create_pool(&db_path).expect("failed to create db pool");
 
     let (to_audio_tx, to_audio_rx) = flume::bounded::<AudioAction>(10);
