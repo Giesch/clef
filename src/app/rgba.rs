e14032a9a104605ceaf740c0bdd5b7990034127a
Dan Gieschen Knutson <Dan.Knutson@gmail.com>
2023-04-24 07:04:11 -0500

    Avoid Image Cloning (#10)
    * store the iced image handle instead of raw bytes
    * this results in cloning an Arc instead of the Vec<u8>

--- src/app/rgba.rs
+++ src/app/rgba.rs
@@ -1,48 +1,59 @@
 use camino::{Utf8Path, Utf8PathBuf};
 use iced::widget::image;
-use image_rs::{imageops::FilterType, ColorType};
+use iced_native::image::Handle;
+use image_rs::Rgba;
+use image_rs::{imageops::FilterType, ColorType, ImageBuffer};
+
+/// The size that album art gets resized (down) to.
+pub const IMAGE_SIZE: u16 = 256;
 
 /// Image pixels in the format that iced converts them to internally
 /// Doing the conversion ahead of time (outside the framework)
-/// makes it possible to do it off the ui thread
+/// makes it possible to do that work off the ui thread.
+/// The slow operations to avoid include both the resize and the format conversion.
 ///
 /// https://github.com/iced-rs/iced/issues/549
 #[derive(Clone)]
 pub struct RgbaBytes {
-    height: u32,
     width: u32,
-    bytes: Vec<u8>,
+    height: u32,
+    handle: Handle,
 }
 
 impl RgbaBytes {
     #[cfg(test)]
     pub fn empty() -> Self {
-        Self {
-            height: 0,
-            width: 0,
-            bytes: Default::default(),
-        }
+        let handle = Handle::from_pixels(0, 0, vec![]);
+        Self { width: 0, height: 0, handle }
+    }
+
+    fn from_buffer(rgba: ImageBuffer<Rgba<u8>, Vec<u8>>) -> Self {
+        let width = rgba.width();
+        let height = rgba.height();
+        let bytes = rgba.into_raw();
+        let handle = Handle::from_pixels(width, height, bytes);
+
+        RgbaBytes { width, height, handle }
     }
 }
 
-impl From<RgbaBytes> for image::Handle {
-    fn from(rgba_bytes: RgbaBytes) -> Self {
-        image::Handle::from_pixels(rgba_bytes.width, rgba_bytes.height, rgba_bytes.bytes)
+impl From<&RgbaBytes> for image::Handle {
+    fn from(rgba_bytes: &RgbaBytes) -> Self {
+        // NOTE The handle uses an Arc internally, so this clone is cheap
+        rgba_bytes.handle.clone()
     }
 }
 
 impl std::fmt::Debug for RgbaBytes {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("RgbaBytes")
-            .field("height", &self.height)
             .field("width", &self.width)
+            .field("height", &self.height)
             .finish()
     }
 }
 
-pub const IMAGE_SIZE: u16 = 256;
-
-/// NOTE this is slow
+// NOTE this is slow
 pub fn load_rgba(path: &Utf8PathBuf) -> anyhow::Result<RgbaBytes> {
     let img = image_rs::open(path)?;
     let img = img.resize(
@@ -51,34 +62,30 @@ pub fn load_rgba(path: &Utf8PathBuf) -> anyhow::Result<RgbaBytes> {
         FilterType::Lanczos3,
     );
 
-    let rgba = img.to_rgba8();
-
-    let rgba_bytes = RgbaBytes {
-        height: rgba.height(),
-        width: rgba.width(),
-        bytes: rgba.into_raw(),
-    };
+    let rgba_bytes = RgbaBytes::from_buffer(img.to_rgba8());
 
     Ok(rgba_bytes)
 }
 
-// NOTE this assumes that the 'conversion'
+// NOTE this assumes that the 'conversion' to rgba8
 // will be fast because it's already in the right format
 pub fn load_cached_rgba_bmp(path: &Utf8Path) -> anyhow::Result<RgbaBytes> {
     let img = image_rs::open(path)?;
-    let rgba = img.to_rgba8();
-
-    let rgba_bytes = RgbaBytes {
-        height: rgba.height(),
-        width: rgba.width(),
-        bytes: rgba.into_raw(),
-    };
+    let rgba_bytes = RgbaBytes::from_buffer(img.to_rgba8());
 
     Ok(rgba_bytes)
 }
 
 pub fn save_rgba(path: &Utf8PathBuf, rgba: &RgbaBytes) -> anyhow::Result<()> {
-    let RgbaBytes { height, width, bytes } = rgba;
+    use iced_native::image::Data;
+
+    let RgbaBytes { width, height, handle } = rgba;
+    let bytes = match handle.data() {
+        Data::Path(_) => unreachable!(),
+        Data::Bytes(bytes) => bytes,
+        Data::Rgba { pixels, .. } => pixels,
+    };
+
     image_rs::save_buffer(path, bytes, *width, *height, ColorType::Rgba8)?;
 
     Ok(())
