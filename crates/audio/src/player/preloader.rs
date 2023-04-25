5eb63c1d7a6a94d52351338a4b3ca598f6e7283d
Dan Knutson <dan.knutson@gmail.com>
2023-04-25 07:34:59 -0500

    decode one packet

--- crates/audio/src/player/preloader.rs
+++ crates/audio/src/player/preloader.rs
@@ -1,14 +1,16 @@
 use std::fs::File;
 use std::thread::JoinHandle;
 
-use anyhow::Context;
+use anyhow::{bail, Context};
 use camino::Utf8PathBuf;
 use flume::{Receiver, RecvError, Sender};
+use symphonia::core::audio::{AsAudioBufferRef, AudioBuffer, AudioBufferRef};
 use symphonia::core::codecs::Decoder;
 use symphonia::core::formats::{FormatOptions, FormatReader};
 use symphonia::core::io::MediaSourceStream;
 use symphonia::core::meta::MetadataOptions;
 use symphonia::core::probe::Hint;
+use symphonia::core::sample::{i24, u24};
 
 use crate::track_info::{first_supported_track, TrackInfo};
 
@@ -33,6 +35,7 @@ pub struct PreloadedContent {
     pub reader: Box<dyn FormatReader>,
     pub decoder: Box<dyn Decoder>,
     pub track_info: TrackInfo,
+    pub predecoded_packets: Vec<AnyAudioBuffer>,
 }
 
 impl std::fmt::Debug for PreloadedContent {
@@ -40,6 +43,7 @@ impl std::fmt::Debug for PreloadedContent {
         f.debug_struct("PreloadedContent")
             .field("path", &self.path)
             .field("track_info", &self.track_info)
+            .field("predecoded_packets", &self.predecoded_packets.len())
             .finish()
     }
 }
@@ -127,19 +131,86 @@ fn prepare_decoder(path: Utf8PathBuf) -> anyhow::Result<PreloadedContent> {
         .format(&hint, mss, &format_opts, &metadata_opts)
         .context("The input was not supported by any format reader")?;
 
-    let track =
-        first_supported_track(probed.format.tracks()).context("no playable track")?;
+    let mut reader = probed.format;
+
+    let track = first_supported_track(reader.tracks()).context("no playable track")?;
     let track_info: TrackInfo = track.into();
 
     // default decode opts (no verify)
-    let decoder = symphonia::default::get_codecs()
+    let mut decoder = symphonia::default::get_codecs()
         .make(&track.codec_params, &Default::default())
         .context("making decoder")?;
 
+    // TODO decoding!
+
+    // TODO handle EOF differently if looping
+    let Ok(packet) = reader.next_packet() else {
+        bail!("failed to read packet");
+    };
+
+    let timestamp = packet.ts();
+
+    let decoded = match decoder.decode(&packet) {
+        Ok(decoded) => AnyAudioBuffer::from_ref(decoded),
+        Err(e) => bail!("failed to decode packet: {e}"),
+    };
+
+    let predecoded_packets = vec![decoded];
+
     Ok(PreloadedContent {
-        reader: probed.format,
+        reader,
         path,
         decoder,
         track_info,
+        predecoded_packets,
     })
+}
+
+// an owned version of AudioBufferRef, so I don't have to deal with generic lifetimes
+#[allow(missing_debug_implementations)]
+pub enum AnyAudioBuffer {
+    U8(AudioBuffer<u8>),
+    U16(AudioBuffer<u16>),
+    U24(AudioBuffer<u24>),
+    U32(AudioBuffer<u32>),
+    S8(AudioBuffer<i8>),
+    S16(AudioBuffer<i16>),
+    S24(AudioBuffer<i24>),
+    S32(AudioBuffer<i32>),
+    F32(AudioBuffer<f32>),
+    F64(AudioBuffer<f64>),
+}
+
+impl AnyAudioBuffer {
+    fn from_ref<'a>(buffer_ref: AudioBufferRef<'a>) -> AnyAudioBuffer {
+        match buffer_ref {
+            AudioBufferRef::U8(sample) => AnyAudioBuffer::U8(sample.into_owned()),
+            AudioBufferRef::U16(sample) => AnyAudioBuffer::U16(sample.into_owned()),
+            AudioBufferRef::U24(sample) => AnyAudioBuffer::U24(sample.into_owned()),
+            AudioBufferRef::U32(sample) => AnyAudioBuffer::U32(sample.into_owned()),
+            AudioBufferRef::S8(sample) => AnyAudioBuffer::S8(sample.into_owned()),
+            AudioBufferRef::S16(sample) => AnyAudioBuffer::S16(sample.into_owned()),
+            AudioBufferRef::S24(sample) => AnyAudioBuffer::S24(sample.into_owned()),
+            AudioBufferRef::S32(sample) => AnyAudioBuffer::S32(sample.into_owned()),
+            AudioBufferRef::F32(sample) => AnyAudioBuffer::F32(sample.into_owned()),
+            AudioBufferRef::F64(sample) => AnyAudioBuffer::F64(sample.into_owned()),
+        }
+    }
+}
+
+impl AsAudioBufferRef for AnyAudioBuffer {
+    fn as_audio_buffer_ref(&self) -> AudioBufferRef<'_> {
+        match self {
+            AnyAudioBuffer::U8(sample) => sample.as_audio_buffer_ref(),
+            AnyAudioBuffer::U16(sample) => sample.as_audio_buffer_ref(),
+            AnyAudioBuffer::U24(sample) => sample.as_audio_buffer_ref(),
+            AnyAudioBuffer::U32(sample) => sample.as_audio_buffer_ref(),
+            AnyAudioBuffer::S8(sample) => sample.as_audio_buffer_ref(),
+            AnyAudioBuffer::S16(sample) => sample.as_audio_buffer_ref(),
+            AnyAudioBuffer::S24(sample) => sample.as_audio_buffer_ref(),
+            AnyAudioBuffer::S32(sample) => sample.as_audio_buffer_ref(),
+            AnyAudioBuffer::F32(sample) => sample.as_audio_buffer_ref(),
+            AnyAudioBuffer::F64(sample) => sample.as_audio_buffer_ref(),
+        }
+    }
 }
